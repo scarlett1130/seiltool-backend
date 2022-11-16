847fc116dea23b91f5a33c0a652234a28ea8637b
thisisthelife0304 <stevenhunt0304@gmail.com>
2022-11-17 00:38:46 +0100

    CROS added

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import json
 from models import SearchData
 import logging
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 logger = logging.getLogger(__name__)
 
