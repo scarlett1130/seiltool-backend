a0bc4d73ce17a73cfbf22336e6afa74101bb9cf9
thisisthelife0304 <stevenhunt0304@gmail.com>
2022-11-18 04:16:19 +0100

    Backend:Completed

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, HTTPException
+from pickle import FALSE, TRUE
+from fastapi import FastAPI, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
 import json
-from models import SearchData
+from models import SearchData, CategoryList
 import logging
 
 app = FastAPI()
@@ -10,51 +11,63 @@ origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins = origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods = ["*"],
+    allow_headers = ["*"],
 )
 
 logger = logging.getLogger(__name__)
 
 @app.get("/", response_model=SearchData)
 async def get_json_data(
-    category: bool=True,
-    description: bool=True,
-    next_inspection: bool=True,
-    operator: bool=True,
-    ordz: bool=True,
-    site: bool=True,
-    page: int=0,
-    limit: int=10
+    categories: str = None,
+    keyword: str = "",
+    page: int = 0,
+    limit: int = 10
 ):
+    categories = json.loads(categories) if categories is not None else []
     data = None
     try:
         with open("search.json", "r") as json_file:
-            data = json.load(json_file)[page * limit: (page + 1) * limit]
-            print (data)
+            data = json.load(json_file)
     except Exception as error:
         logger.error(f"Error reading json file: {error}")
 
     if data:
-        json_data = []
-        for row in data:
-            row_data = {}
-            if category:
-                row_data['category'] = row['category']
-            if description:
-                row_data['description'] = row['description']
-            if next_inspection:
-                row_data['next_inspection'] = row['next_inspection']
-            if operator:
-                row_data['operator'] = row['operator']
-            if ordz:
-                row_data['ordz'] = row['ordz']
-            if site:
-                row_data['site'] = row['site']
-            json_data.append(row_data)
+        if categories == [] and keyword == "":
+            return { "data": data[page * limit: (page + 1) * limit], "total": len(data) }
+        else:
+            tmp = []
+            for row in data:
+                search_string = ""
+                for item in row:
+                    search_string += str(row[item]['content']) + "##"
+                if search_string.__contains__(keyword) and (row['category']['content'] in categories or categories == []):
+                    tmp.append(row)
 
-        return { "data": json_data }
+            return { "data": tmp[page * limit: (page + 1) * limit], "total": len(tmp) }
+
+    raise HTTPException(400, "Bad Request")
+
+@app.get("/categories", response_model=CategoryList)
+async def get():
+    data = None
+    try:
+        with open("search.json", "r") as json_file:
+            data = json.load(json_file)
+    except Exception as error:
+        logger.error(f"Error reading json file: {error}")
+    if data:
+        obj_map = {}
+        for row in data:
+            if not obj_map.__contains__(row["category"]["content"]):
+                obj_map[row["category"]["content"]] = row["category"]["content"]
+        categories = obj_map.values()
+
+        def keyToCode(k):
+            return k['code']
+
+        return { "data": sorted([{ "code": category } for category in categories if category is not None], key = keyToCode) }
 
     raise HTTPException(400, "Bad Request")
--- models.py
+++ models.py
@@ -7,16 +7,21 @@ class ValidItem(BaseModel):
     content: Optional[str]
     link_type: Optional[str]
 
-
 class Item(BaseModel):
     category: Optional[ValidItem]
     description: Optional[ValidItem]
     next_inspection: Optional[ValidItem]
+    operating_manager: Optional[ValidItem]
     operator: Optional[ValidItem]
     ordz: Optional[ValidItem]
     site: Optional[ValidItem]
 
-
 class SearchData(BaseModel):
     data: List[Item]
-    
+    total: Optional[int]
+
+class CategoryItem(BaseModel):
+    code: Optional[str]
+
+class CategoryList(BaseModel):
+    data: List[CategoryItem]
